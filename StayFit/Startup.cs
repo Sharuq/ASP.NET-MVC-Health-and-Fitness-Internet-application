283b9689da614f6dfa5b6554fcfed7c2ef49b02a
unknown <mkun0005@student.monash.edu>
2018-09-12 19:47:09 +1000

    commit_msg

---
+++ StayFit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StayFit.Startup))]
+namespace StayFit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
